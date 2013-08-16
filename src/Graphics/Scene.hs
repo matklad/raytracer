81e76286fe0eb1cae6cd763e15bbcf8c253985b2
Sergei Lebedev <superbobry@gmail.com>
2013-08-16 17:52:41 +0400

    Changed 'trace' signature as suggested
      >                 ,                           ,
      >                      ,
      >                     ,

--- src/Graphics/Scene.hs
+++ src/Graphics/Scene.hs
@@ -2,10 +2,12 @@ module Graphics.Scene
     ( Scene(..)
     ) where
 
+import Data.Colour (Colour)
 import Graphics.Camera (Camera)
 import Graphics.Shape (SomeShape)
 
 data Scene = Scene
     { sceneCamera :: !Camera
     , sceneShapes :: ![SomeShape]
+    , sceneColour :: !Colour
     }
--- src/Graphics/Tracer.hs
+++ src/Graphics/Tracer.hs
@@ -1,9 +1,9 @@
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE NamedFieldPuns #-}
 
 module Graphics.Tracer
     ( trace
-    , shade
     , render
     ) where
 
@@ -12,31 +12,35 @@ import Data.Function (on)
 import Data.List (minimumBy)
 import Data.Maybe (mapMaybe)
 
-import Data.Colour (Colour, black)
+import Data.Colour (Colour)
 import Data.Ray (Ray, applyRay)
 import Data.Vec (Vec)
 import Graphics.Camera (applyCamera)
 import Graphics.Scene (Scene(..))
 import Graphics.Shape (Shape(..), SomeShape, colourAt)
 
-trace :: Ray -> [SomeShape] -> Maybe (SomeShape, Vec)
-trace ray shapes = case candidates of
-    [] -> Nothing
-    _  -> case minimumBy (compare `on` snd) candidates of
-        (shape, d) -> Just (shape, applyRay ray d)
+intersections :: [SomeShape] -> Ray -> [(SomeShape, Double)]
+intersections shapes ray =
+    mapMaybe (\shape -> (shape, ) <$> shape `intersect` ray) shapes
+{-# INLINE intersections #-}
+
+trace :: Scene -> Ray -> Colour
+trace scene@(Scene { .. }) ray = shade scene ray $ do
+    (shape, d) <- closest
+    return (shape, applyRay ray d)
   where
-    candidates :: [(SomeShape, Double)]
-    candidates =
-      mapMaybe (\shape -> (shape, ) <$> shape `intersect` ray) shapes
+    closest :: Maybe (SomeShape, Double)
+    closest = case intersections sceneShapes ray of
+        []          -> Nothing
+        candidates  -> Just $ minimumBy (compare `on` snd) candidates
 {-# INLINEABLE trace #-}
 
-shade :: Ray -> Maybe (SomeShape, Vec) -> Colour
-shade _ray (Just (shape, v)) = shape `colourAt` v
-shade _ray Nothing = black
+shade :: Scene -> Ray -> Maybe (SomeShape, Vec) -> Colour
+shade (Scene { sceneColour }) _ray = maybe sceneColour (uncurry colourAt)
 {-# INLINABLE shade #-}
 
 render :: Scene -> (Int, Int) -> Colour
-render (Scene { .. }) p = shade ray $ trace ray sceneShapes where
+render scene@(Scene { .. }) p = trace scene ray where
   ray :: Ray
   ray = applyCamera sceneCamera p
 {-# INLINEABLE render #-}
